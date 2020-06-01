85e9e5a10b3643a1cdf467f53ee86d5a244928ab
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-06-01 10:37:08 -0300

    atualizações no desafio csharp-1

--- csharp-1/Source/SoccerTeamsManager.cs
+++ csharp-1/Source/SoccerTeamsManager.cs
@@ -9,8 +9,8 @@ namespace Codenation.Challenge
     public class SoccerTeamsManager : IManageSoccerTeams
     {
 
-        private List<Team> Teams { get; set; } = new List<Team>();
-        public List<Player> Players { get; set; } = new List<Player>();
+        private List<Team> _teams = new List<Team>();
+        private List<Player> _players  = new List<Player>();
 
         public SoccerTeamsManager()
         {
@@ -19,24 +19,24 @@ namespace Codenation.Challenge
         public void AddTeam(long id, string name, DateTime createDate, string mainShirtColor, string secondaryShirtColor)
         {
             CheckTeamInUse(id);
-            Teams.Add(new Team(id, name, createDate, mainShirtColor, secondaryShirtColor));
+            _teams.Add(new Team(id, name, createDate, mainShirtColor, secondaryShirtColor));
         }
 
         public void AddPlayer(long id, long teamId, string name, DateTime birthDate, int skillLevel, decimal salary)
         {
             ChecPlayerInUse(id);
             CheckTeamExist(teamId);
-            Players.Add(new Player(id, teamId, name, birthDate, skillLevel, salary));
+            _players.Add(new Player(id, teamId, name, birthDate, skillLevel, salary));
         }
 
         public void SetCaptain(long playerId)
         {
             CheckPlayerExist(playerId);
             
-            foreach (Player p in Players)
+            foreach (Player p in _players)
                     if (p.Id == playerId)
                     {
-                        foreach (Team t in Teams)
+                        foreach (Team t in _teams)
                         {
                         if (t.Id == p.TeamId)
                         {
@@ -51,50 +51,50 @@ namespace Codenation.Challenge
         {
             CheckTeamExist(teamId);
             CheckCapitainExist(teamId);
-            return Teams.Where(x => x.Id == teamId).Select(x =>x.Captain.PlayerId).SingleOrDefault();
+            return _teams.Where(x => x.Id == teamId).Select(x =>x.Captain.PlayerId).SingleOrDefault();
         }
 
         public string GetPlayerName(long playerId)
         {
             CheckPlayerExist(playerId);
-            return Players.Where(x => x.Id == playerId).First().Name;
+            return _players.Where(x => x.Id == playerId).First().Name;
         }
 
         public string GetTeamName(long teamId)
         {
             CheckTeamExist(teamId);
-            return Teams.Where(x => x.Id == teamId).Select(x => x.Name).FirstOrDefault();
+            return _teams.Where(x => x.Id == teamId).Select(x => x.Name).FirstOrDefault();
         }
 
         public List<long> GetTeamPlayers(long teamId)
         {
             CheckTeamExist(teamId);
-            return Players.Where(p => p.TeamId == teamId).OrderBy(p => p.Id).Select(p => p.Id).ToList();
+            return _players.Where(p => p.TeamId == teamId).OrderBy(p => p.Id).Select(p => p.Id).ToList();
         } 
         public long GetBestTeamPlayer(long teamId)
         {
             CheckTeamExist(teamId);
-            return Players.Where(x => x.TeamId == teamId).OrderByDescending(x => x.SkillLevel).ThenBy(x => x.Id).Select(x => x.Id).FirstOrDefault();
+            return _players.Where(x => x.TeamId == teamId).OrderByDescending(x => x.SkillLevel).ThenBy(x => x.Id).Select(x => x.Id).FirstOrDefault();
          }
 
         public long GetOlderTeamPlayer(long teamId)
         {
 
             CheckTeamExist(teamId);
-            return Players.Where(x => x.TeamId == teamId).
+            return _players.Where(x => x.TeamId == teamId).
                 OrderBy(x => x.BirthDate).ThenBy(x => x.Id).
                 Select(x=>x.Id).FirstOrDefault();
         }
 
         public List<long> GetTeams()
         {
-            return Teams.OrderBy(x=>x.Id).Select(x => x.Id).ToList();
+            return _teams.OrderBy(x=>x.Id).Select(x => x.Id).ToList();
         }
 
         public long GetHigherSalaryPlayer(long teamId)
         {
             CheckTeamExist(teamId);
-            return Players.Where(x => x.TeamId == teamId).
+            return _players.Where(x => x.TeamId == teamId).
             OrderByDescending(x => x.Salary).ThenBy(x=>x.Id).
             Select(x => x.Id).FirstOrDefault(); 
          }
@@ -102,12 +102,12 @@ namespace Codenation.Challenge
         public decimal GetPlayerSalary(long playerId)
         {
             CheckPlayerExist(playerId);
-            return Players.Where(x => x.Id == playerId).FirstOrDefault().Salary;
+            return _players.Where(x => x.Id == playerId).FirstOrDefault().Salary;
         }
 
         public List<long> GetTopPlayers(int top)
         {
-            return Players.OrderByDescending(x => x.SkillLevel).
+            return _players.OrderByDescending(x => x.SkillLevel).
                 ThenBy(x => x.Id).Select(x=>x.Id).Take(top).ToList(); 
         }
 
@@ -116,10 +116,10 @@ namespace Codenation.Challenge
             CheckTeamExist(teamId);
             CheckTeamExist(visitorTeamId);
             string shirtColor;
-            var home = Teams.Where(x => x.Id == teamId).
+            var home = _teams.Where(x => x.Id == teamId).
                 Select(x => new { x.CorUniformePrincipal, x.CorUniformeSecundario }).
                 FirstOrDefault();
-            var visitor = Teams.Where(x => x.Id == visitorTeamId).
+            var visitor = _teams.Where(x => x.Id == visitorTeamId).
                 Select(x => new { x.CorUniformePrincipal, x.CorUniformeSecundario }).
                 FirstOrDefault();
             if (home.CorUniformePrincipal == visitor.CorUniformePrincipal)
@@ -129,30 +129,30 @@ namespace Codenation.Challenge
             return shirtColor;
         }
 
-        //validaçoes
+        //validations
         private void CheckTeamInUse(long id)
         {
-            if (Teams.Any(x => x.Id == id))
+            if (_teams.Any(x => x.Id == id))
                 throw new UniqueIdentifierException("Error! this Id is in used. ");
         }
         private void ChecPlayerInUse(long id)
         {
-            if ((Players.Any(p => p.Id == id)))
+            if ((_players.Any(p => p.Id == id)))
                 throw new UniqueIdentifierException("Error!The Player Id was not found.");
         }
         private void CheckPlayerExist(long id)
         {
-            if (!(Players.Any(p => p.Id == id)))
+            if (!(_players.Any(p => p.Id == id)))
                 throw new PlayerNotFoundException("Error!The Player Id was not found.");
         }
         private void CheckTeamExist(long id)
         {
-            if (!(Teams.Any(p => p.Id == id)))
+            if (!(_teams.Any(p => p.Id == id)))
                 throw new TeamNotFoundException("Error! Team Id was not found. ");
         }
         private void CheckCapitainExist(long id)
         {
-            if (Teams.Where(x => x.Id == id).First().Captain==null)   //Caso o time informado não tenha um capitão, retornar
+            if (_teams.Where(x => x.Id == id).First().Captain==null)   
                 throw new CaptainNotFoundException("Error! Captain was not found. ");
         }
     }
